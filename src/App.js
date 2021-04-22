3832c7d732606988015c36348e7bf5e2567a5487
Anton Babenko <51702428+Dewwine@users.noreply.github.com>
2021-04-22 12:42:22 +0300

    cahnge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
-
+import Footer from './components/Footer/Footer';
 const App = (props) => {
   return (
     <div className='app-wrapper'>
       <Header />
+      <Footer />
     </div>
   )
 }
